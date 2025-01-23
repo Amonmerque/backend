aacbbb9d4b628850a298d07478b86a2362362094
Alexandre Monmerque <alexandre.monmerque@free.fr>
2025-01-23 23:52:47 +0100

    Add env variables

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
- SECRET_KEY = os.getenv("SECRET_KEY")
- SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
- SQLALCHEMY_TRACK_MODIFICATIONS = False
+#  SECRET_KEY = os.getenv("SECRET_KEY")
+#  SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+#  SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SECRET_KEY = "npg_0b5hXqkCUHtu"
+  SQLALCHEMY_DATABASE_URI = "postgresql://neondb_owner:npg_0b5hXqkCUHtu@ep-old-mountain-a25cfcsg-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require"
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
