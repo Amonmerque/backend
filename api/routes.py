f96365a66e9405d57bd265da62d0a0b8d143e84d
Alexandre Monmerque <alexandre.monmerque@free.fr>
2025-01-26 00:15:49 +0100

    Updates

--- api/routes.py
+++ api/routes.py
@@ -1,12 +1,14 @@
 from flask import request, make_response, session
 from flask_restful import Resource
 from flask_cors import CORS, cross_origin
-from api import app, db, api, bcrypt
+from api import app, db
 from flask import render_template
 
-# from api.models import User, Movie, CartItem, Review
 from api.models import Users
-# from api.models import Movie, Users
+
+def request_all_users():
+  users = Users.query.all()
+  return [user.to_dict() for user in users]
 
 # Views go here!
 @app.route("/")
@@ -32,9 +34,7 @@ def user_create():
     user = Users(nom=d_nom, prenom=d_prenom, solde=d_solde)
     db.session.add(user)
     db.session.commit()
-    users = Users.query.all()
-    body = [user.to_dict() for user in users]
-    return make_response(body, 200)
+    return make_response(request_all_users(), 200)
   else:
     return make_response(dict(), 404)
 
@@ -44,15 +44,41 @@ def take_beer():
   if request.method == "POST":
     data=request.get_json()
     d_id=data['id']
+    d_nb=data['nb']
     user = Users.query.filter_by(id=d_id).first()
-    if user.solde >= 2:
-      user.solde = user.solde-2
+    if user.solde >= d_nb * 2:
+      user.solde = user.solde-(d_nb * 2)
       db.session.commit()
-      users = Users.query.all()
-      body = [user.to_dict() for user in users]
-      return make_response(body, 200)
+      return {"solde": user.solde}, 200
     else:
-      return {"error": "Invalid email"}, 400
+      return {"error": "Error during beer"}, 400
+    
+@app.route("/user/soda", methods=["POST"])
+@cross_origin()
+def take_soda():
+  if request.method == "POST":
+    data=request.get_json()
+    d_id=data['id']
+    d_nb=data['nb']
+    user = Users.query.filter_by(id=d_id).first()
+    if user.solde >= 1 * d_nb:
+      user.solde = user.solde-(1 * d_nb)
+      db.session.commit()
+      return {"solde": user.solde}, 200
+    else:
+      return {"error": "Error during soda"}, 400
+    
+@app.route("/user/recharge", methods=["POST"])
+@cross_origin()
+def recharge():
+  if request.method == "POST":
+    data=request.get_json()
+    d_id=data['id']
+    d_ajout=data['ajout']
+    user = Users.query.filter_by(id=d_id).first()
+    user.solde = user.solde+d_ajout
+    db.session.commit()
+    return {"solde": user.solde}, 200
     
 @app.route("/user/delete", methods=["POST"])
 @cross_origin()
@@ -64,8 +90,6 @@ def delete_user():
     print(user.id)
     db.session.delete(user)
     db.session.commit()
-    users = Users.query.all()
-    body = [user.to_dict() for user in users]
-    return make_response(body, 200)
+    return make_response(request_all_users(), 200)
 
   
